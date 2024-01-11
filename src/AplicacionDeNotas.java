c1908cac80e176e3b1d70b732d56fd4bc7b7e564
yasmin <yasminquinonez11@gmail.com>
2024-01-10 21:30:37 -0600

    Aplicacion java

---
+++ src/AplicacionDeNotas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author yasmi
+ */
+public class AplicacionDeNotas {
+    public static void main(String[]args) {
+         NewJFrame color=new  NewJFrame();
+         color.setVisible(true) ;       
+    }
+   
+}
